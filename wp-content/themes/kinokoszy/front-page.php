8d81fae6dd061ac2ae309d46ef20c652fe38b960
Lucas Källberg <lucas.arne@hotmail.com>
2023-03-27 15:37:00 +0200

    worked on front-page

--- wp-content/themes/kinokoszy/front-page.php
+++ wp-content/themes/kinokoszy/front-page.php
@@ -1,3 +1,16 @@
-<?php // landningssida
+<!DOCTYPE html>
 
-get_header();
+<head>
+    <?php wp_head(); ?>
+</head>
+
+<main class="front-page-main">
+
+    <div class="front-page-kino-section">
+        <h1>Kino Koszyk</h1>
+    </div>
+    <div class="front-page-joanna-section">
+        <h1>JOANNA HELANDER</h1>
+    </div>
+
+</Main>
--- wp-content/themes/kinokoszy/functions.php
+++ wp-content/themes/kinokoszy/functions.php
@@ -37,6 +37,8 @@ add_action('init', function () {
 });
 
 function custom_styles() {
-	wp_enqueue_style( 'footer', get_template_directory_uri() . '/assets/footer.css' );
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+	wp_enqueue_style( 'footer', get_template_directory_uri() . '/assets/css/footer.css' );
+    wp_enqueue_style( 'front-page', get_template_directory_uri() . '/assets/css/front-page.css' );
 }
 add_action( 'wp_enqueue_scripts', 'custom_styles' );
