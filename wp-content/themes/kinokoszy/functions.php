cced3b22ba8423f0dae6f57a1bb732ba2a3f9a2c
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2023-03-27 10:38:48 +0200

    post-types

--- wp-content/themes/kinokoszy/functions.php
+++ wp-content/themes/kinokoszy/functions.php
@@ -1,10 +1,39 @@
 <?php
 
 add_action('init', function (){
-    // Create new custom type "Ice cream", needs to be its own context so we create it our self 
-    register_post_type('films', array(
+    // Create new custom type "films"
+    register_post_type('film', array(
         'labels' => array(
-            'name' => __('Films'),
+            'name' => __('Film'),
+        ),
+        'public'=> true,
+        'has_archive' => true
+    ));
+    
+  
+        
+});
+ // Create new custom type "books"
+add_action('init', function (){
+   
+    register_post_type('book', array(
+        'labels' => array(
+            'name' => __('Book'),
+        ),
+        'public'=> true,
+        'has_archive' => true
+    ));
+    
+  
+        
+});
+
+ // Create new custom type "books"
+ add_action('init', function (){
+   
+    register_post_type('exhibition', array(
+        'labels' => array(
+            'name' => __('Exhibition'),
         ),
         'public'=> true,
         'has_archive' => true
---
+++ wp-content/themes/kinokoszy/single-film.php
@@ -0,0 +1,2 @@
+<?php
+// single-post for films 
