0b0eb4c80a521292021a3535a5e28c252a597bd7
jaken92 <jaaakeeen@gmail.com>
2023-03-30 09:37:51 +0200

    title added to header

--- wp-content/themes/kinokoszy/header.php
+++ wp-content/themes/kinokoszy/header.php
@@ -13,20 +13,24 @@ $joanna_menu = wp_get_nav_menu_items('joanna-nav-menu');
 
     <header>
         <nav class="test">
+            <div>
+                <?php if (is_page('home-kino') || is_page('contact-kino') || is_page('about-kino') || is_page('films')) : ?>
+                    <?php foreach ($kino_menu as $link) : ?>
+                        <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>" href="<?= $link->url; ?>"><?= $link->title; ?></a>
+                    <?php endforeach; ?>
 
-            <?php if (is_page('home-kino') || is_page('contact-kino') || is_page('about-kino') || is_page('films')) : ?>
-            <?php foreach ($kino_menu as $link) : ?>
-            <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>"
-                href="<?= $link->url; ?>"><?= $link->title; ?></a>
-            <?php endforeach; ?>
+
+                <?php else : ?>
+                    <?php foreach ($joanna_menu as $link) : ?>
+                        <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>" href="<?= $link->url; ?>"><?= $link->title; ?></a>
+                    <?php endforeach; ?>
+                <?php endif ?>
+            </div>
+            <div>
+                <h1><?php WP_title(); ?></h1>
+            </div>
 
 
-            <?php else : ?>
-            <?php foreach ($joanna_menu as $link) : ?>
-            <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>"
-                href="<?= $link->url; ?>"><?= $link->title; ?></a>
-            <?php endforeach; ?>
-            <?php endif ?>
         </nav>
     </header>
 
