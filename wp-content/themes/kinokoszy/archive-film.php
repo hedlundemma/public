0ef689ce09f5add8e46ad4a59a4d0a16f348d742
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-04-03 21:50:20 +0200

    Embedded movie trailer.
    Added the movie trailer in an "iframe" element.

--- wp-content/themes/kinokoszy/archive-film.php
+++ wp-content/themes/kinokoszy/archive-film.php
@@ -12,22 +12,18 @@
     </div>
 </section>
 
-<section class="hero-films"></section>
+<section class="hero-films">
+    <iframe class="responsive-iframe" src="//player.vimeo.com/video/150078754" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+</section>
 
 <section class="film-selection-flex-images">
     <div class="image-row">
-        <div class="image-container"><img
-                src="/wp-content/themes/kinokoszy/assets/images/films/Optimized-ach-jakze-szwecja-avdammad.png" alt=""
-                srcset=""></div>
-        <div class="image-container"><img
-                src="/wp-content/themes/kinokoszy/assets/images/films/Optimized-poster_the_return.png" alt="" srcset="">
+        <div class="image-container"><img src="/wp-content/themes/kinokoszy/assets/images/films/Optimized-ach-jakze-szwecja-avdammad.png" alt="" srcset=""></div>
+        <div class="image-container"><img src="/wp-content/themes/kinokoszy/assets/images/films/Optimized-poster_the_return.png" alt="" srcset="">
         </div>
-        <div class="image-container"><img
-                src="/wp-content/themes/kinokoszy/assets/images/films/Optimized-T8D_affisch.png" alt="" srcset="">
+        <div class="image-container"><img src="/wp-content/themes/kinokoszy/assets/images/films/Optimized-T8D_affisch.png" alt="" srcset="">
         </div>
-        <div class="image-container"><img
-                src="/wp-content/themes/kinokoszy/assets/images/films/Optimized-Tvillingarna från Krakow med cellisten.png"
-                alt="" srcset=""></div>
+        <div class="image-container"><img src="/wp-content/themes/kinokoszy/assets/images/films/Optimized-Tvillingarna från Krakow med cellisten.png" alt="" srcset=""></div>
     </div>
 </section>
 
@@ -37,31 +33,31 @@
 
 
 
-<?php while (have_posts()) : the_post();
+    <?php while (have_posts()) : the_post();
 
         the_title();
 
 
         $image = get_field('image');
         if (!empty($image)) : ?>
-<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-<?php endif; ?>
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+        <?php endif; ?>
 
 
 
-<?php
+        <?php
         $link = get_field('url');
         if ($link) : ?>
-<a class="button" href="<?php echo esc_url($link); ?>">Trailer</a>
-<?php endif; ?>
+            <a class="button" href="<?php echo esc_url($link); ?>">Trailer</a>
+        <?php endif; ?>
 
 
 
 
-<a href="<?php the_permalink(); ?>">title</a>
+        <a href="<?php the_permalink(); ?>">title</a>
 
 <?php
-endwhile;
+    endwhile;
 endif;
 
 
