093e0379bbada0f716d7c0a6031a577ccca8040e
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-04-11 14:03:19 +0200

    Fixed file paths on "Contact" -page.

--- wp-content/themes/kinokoszy/contact.php
+++ wp-content/themes/kinokoszy/contact.php
@@ -3,7 +3,7 @@
 <?php get_header(); ?>
 <section class="hero-section">
     <div class="arrow-container">
-        <img src="/wp-content/themes/kinokoszy/assets/images/Group 6.png" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Group 6.png" alt="">
     </div>
     <div class="title-container">
         <h1>CONTACT</h1>
@@ -14,9 +14,9 @@
 <section class="contact-heading-section">
     <h2 class="contact-heading">HAVE A QUESTION? </h2>
     <?php if (is_page('kino/contact')) : ?>
-    <h2 class="contact-heading">CONTACT US!</h2>
+        <h2 class="contact-heading">CONTACT US!</h2>
     <?php else : ?>
-    <h2 class="contact-heading">CONTACT ME!</h2>
+        <h2 class="contact-heading">CONTACT ME!</h2>
     <?php endif ?>
 
 </section>
@@ -24,32 +24,32 @@
 
 <?php the_content() ?>
 <?php if (is_page('kino/contact')) : ?>
-<section class="images-section">
-    <div class="image-container-one">
-        <img src="/wp-content/themes/kinokoszy/assets/images/contact-bo.jpg" alt="Bo persson" />
-        <div class="image-paragraphs-contact">
-            <p>//</p>
-            <p> BO PERSSON </p>
+    <section class="images-section">
+        <div class="image-container-one">
+            <img src="" <?php echo get_template_directory_uri(); ?>/assets/images/contact-bo.jpg" alt="Bo persson" />
+            <div class="image-paragraphs-contact">
+                <p>//</p>
+                <p> BO PERSSON </p>
+            </div>
         </div>
-    </div>
-    <div class="image-container-two">
-        <img src="/wp-content/themes/kinokoszy/assets/images/contact-joanna.jpg" alt="Joanna Helander" />
-        <div class="image-paragraphs-contact">
-            <p>//</p>
-            <p> JOANNA HELANDER</p>
-        </div>
-</section>
+        <div class="image-container-two">
+            <img src="" <?php echo get_template_directory_uri(); ?>/assets/images/contact-joanna.jpg" alt="Joanna Helander" />
+            <div class="image-paragraphs-contact">
+                <p>//</p>
+                <p> JOANNA HELANDER</p>
+            </div>
+    </section>
 <?php else : ?>
-<section class="image-section-only-joanna">
-    <div class="image-container-one">
-        <img src="/wp-content/themes/kinokoszy/assets/images/contact-joanna.jpg" alt="Joanna Helander" />
-        <div class="image-paragraphs-contact">
-            <p>//</p>
-            <p>JOANNA HELANDER</p>
+    <section class="image-section-only-joanna">
+        <div class="image-container-one">
+            <img src="" <?php echo get_template_directory_uri(); ?>/assets/images/contact-joanna.jpg" alt="Joanna Helander" />
+            <div class="image-paragraphs-contact">
+                <p>//</p>
+                <p>JOANNA HELANDER</p>
+            </div>
         </div>
-    </div>
     <?php endif ?>
-</section>
+    </section>
 
-<?php
+    <?php
     get_footer(); ?>
