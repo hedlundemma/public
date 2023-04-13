24aae1f40ffb96f283ce5e6c1c29e6a45f50d9ce
Cellmon95 <lucas.arne@hotmail.com>
2023-04-13 16:59:09 +0200

    photos

---
+++ wp-content/themes/kinokoszy/404.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+?>
+
+<section class="hero-section">
+    <div class="arrow-container">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Group 6.png" alt="">
+    </div>
+    <div class="title-container">
+        <h1>
+            404
+        </h1>
+    </div>
+
+</section>
+
+<?php wp_footer();
+get_footer(); ?>
