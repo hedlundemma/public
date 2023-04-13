a6bae997509418a1f4ca39eafd4ccdfcba308d12
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2023-04-13 12:28:40 +0200

    books/header/footer

--- wp-content/themes/kinokoszy/footer.php
+++ wp-content/themes/kinokoszy/footer.php
@@ -4,28 +4,33 @@ $kino_footer_menu = wp_get_nav_menu_items('kino-footer-menu');
 ?>
 <footer>
     <section class="footer-section">
+        <?php if (is_page('kino') || is_page('kino/contact') || is_page('kino/about') || (get_post_type() == 'film')) : ?>
         <div class="logo-footer">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="logo kinokoszy" width="180px" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="logo kinokoszy"
+                width="180px" />
             <h2> KINO</h2>
             <h3> KOSZYK</h3>
 
         </div>
+        <?php endif?>
         <div class="menu-footer">
             <?php if (is_page('kino') || is_page('kino/contact') || is_page('kino/about') || (get_post_type() == 'film')) : ?>
-                <?php foreach ($kino_footer_menu as $link) : ?>
-                    <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>" href="<?= $link->url; ?>"><?= $link->title; ?></a>
-                <?php endforeach; ?>
+            <?php foreach ($kino_footer_menu as $link) : ?>
+            <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>"
+                href="<?= $link->url; ?>"><?= $link->title; ?></a>
+            <?php endforeach; ?>
 
 
             <?php else : ?>
-                <?php foreach ($joanna_footer_menu as $link) : ?>
-                    <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>" href="<?= $link->url; ?>"><?= $link->title; ?></a>
-                <?php endforeach; ?>
+            <?php foreach ($joanna_footer_menu as $link) : ?>
+            <a title="<?= $link->title; ?>" class="<?= $current_page_id == $link->object_id ? 'underline' : ''; ?>"
+                href="<?= $link->url; ?>"><?= $link->title; ?></a>
+            <?php endforeach; ?>
             <?php endif ?>
         </div>
         <div class="info-section-footer">
             <?php if (is_page('kino') || is_page('kino/contact') || is_page('kino/about') || (get_post_type() == 'film')) : ?>
-                <p>BO PERSSON</p>
+            <p>BO PERSSON</p>
             <?php endif ?>
             <p>JOANNA HELANDER </p>
             <p>KINO.KOSZYK@GMAIL.COM</p>
@@ -34,9 +39,11 @@ $kino_footer_menu = wp_get_nav_menu_items('kino-footer-menu');
 
     </section>
     <section class="lower-footer-section">
+        <?php if (is_page('kino') || is_page('kino/contact') || is_page('kino/about') || (get_post_type() == 'film')) : ?>
         <div class="secondary-footer-logo">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/seclogo.png" alt="logo kinokoszy" />
         </div>
+        <?php endif?>
 
         <div class="copy-section">
             <p> Copyright © 2023 · All Rights Reserved</p>
@@ -44,7 +51,8 @@ $kino_footer_menu = wp_get_nav_menu_items('kino-footer-menu');
         <div class="facebook-logo">
             <a href="https://www.facebook.com/" class="svg">
 
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook-ikon.svg" alt="facebook logo link" /></a>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook-ikon.svg"
+                    alt="facebook logo link" /></a>
             </a>
         </div>
 
